4f4f48d669ea7639cad63a19b96fa0aad5222bd0
flavia0865 <160143425+flavia0865@users.noreply.github.com>
2025-08-19 14:42:33 -0300

    19/8

--- main.js
+++ main.js
@@ -22,6 +22,24 @@ function aumentaTamanho(){
     numeroSenha.textContent=tamanhoSenha;
     geraSenha();
 }
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiusculas ='ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuwxyz';
+const numeros = '0123456789';
+const simbolos = '!@#$%&*';
+geraSenha();
+
+function geraSenha(){
+    let senha = '';
+    for (let i=0; i<tamanhoSenha; i++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
+
+    }
+    campoSenha.value = senha;
+}
 
 
 
