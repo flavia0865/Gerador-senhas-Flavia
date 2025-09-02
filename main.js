a64254a145161cc6a541d42408837835fb6e54c0
flavia0865 <160143425+flavia0865@users.noreply.github.com>
2025-09-02 14:29:15 -0300

    02/09

--- main.js
+++ main.js
@@ -67,7 +67,7 @@ function geraSenha() {
 
 function classificaSenha(tamanhoAlfabeto) {
    let entropia = tamanhoSenha*Math.log2(tamanhoSenha);
-   
+
     forcaSenha.classList.remove('fraca', 'media', 'forte');
     if (entropia > 57) {
         forcaSenha.classList.add('forte');
@@ -77,7 +77,7 @@ function classificaSenha(tamanhoAlfabeto) {
         forcaSenha.classList.add('fraca');
     }
     const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent =  Math.floor(2**entropia/(100e6*60*60*24));
+    valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) +"dias para descobrir essa senha.";
 }
 
 
