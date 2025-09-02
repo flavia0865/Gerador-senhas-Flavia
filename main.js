9cd7cad5897ed8b8deceb85d77815d025ea450d9
flavia0865 <160143425+flavia0865@users.noreply.github.com>
2025-09-02 14:20:44 -0300

    02/09

--- main.js
+++ main.js
@@ -67,9 +67,7 @@ function geraSenha() {
 
 function classificaSenha(tamanhoAlfabeto) {
    let entropia = tamanhoSenha*Math.log2(tamanhoSenha);
-
-
-
+   
     forcaSenha.classList.remove('fraca', 'media', 'forte');
     if (entropia > 57) {
         forcaSenha.classList.add('forte');
